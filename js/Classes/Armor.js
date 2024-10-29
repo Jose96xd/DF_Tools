91072b1c58c3a9e73e6a7699f809911a95d27a7f
José Antonio <jose96xd@gmail.com>
2024-10-29 10:43:00 +0100

    Added clothing characteristics

--- js/Classes/Armor.js
+++ js/Classes/Armor.js
@@ -1,11 +1,20 @@
 import { BasicElement } from "./BasicElement.js";
+import { Material } from "./Material.js";
 
 export class Armor extends BasicElement{
-    constructor({id, name=null, quality=1, material, is_rigid=true}={}){
+    constructor({id, name=null, quality=1, material=new Material(), is_rigid=true, armor_type, layer, coverage, layer_size, layer_permit, is_shaped, ubstep=0, lbstep=0, upstep=0}={}){
         super({id, name});
         this.quality = quality;
         this.material = material;
-        this.is_rigid = is_rigid;
+        this.armor_type = armor_type;
+        this.layer = layer;
+        this.coverage = coverage;
+        this.layer_size = layer_size;
+        this.layer_permit = layer_permit;
+        this.is_shaped = is_shaped;
+        this.ubstep = ubstep;
+        this.lbstep = lbstep;
+        this.upstep = upstep;
     }
     momentum_reduction(momentum, blunt_attack, successful_penetration){
         let final_momentum = momentum - (momentum * 0.05);
@@ -17,5 +26,22 @@ export class Armor extends BasicElement{
             }
         }
         return final_momentum;
+    }
+    getVolume(underlyingSize){
+        const ubstepValue = (this.ubstep === "MAX") ? 3 : parseInt(this.ubstep);
+        const lbstepValue = (this.lbstep === "MAX") ? 3 : parseInt(this.lbstep);
+        const upstepValue = (this.upstep === "MAX") ? 3 : parseInt(this.upstep);
+        const sizeMultiplier = (this.coverage / 100) * (this.layer_size / 100) * (1 + (ubstepValue + lbstepValue + upstepValue) / 4)
+        const size = underlyingSize * sizeMultiplier;
+        return size;
+    }
+    static from_csv(line){
+        const piece = new Armor({
+            id:line[0], name:line[1], layer:line[2], coverage:line[3],
+            layer_size:line[4], layer_permit:line[5], is_shaped:(line[6] === "True"),
+            ubstep:line[7], lbstep:line[8], upstep:line[9],
+            armor_type:line[line.length-1]
+        });
+        return piece;
     }
 }
