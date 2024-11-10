ae9f60f0db2b8ffe5fc6a530dafa736af4edaf7c
José Antonio <jose96xd@gmail.com>
2024-11-10 13:06:20 +0100

    Raw generation functionality added

--- js/Modules/BuildingASCIIVisualizer_page.js
+++ js/Modules/BuildingASCIIVisualizer_page.js
@@ -1,5 +1,6 @@
 import { ColorTranslator } from "../DF_Related/ColorTranslator.js";
 import { createInputField, Table } from "../Web/Web_Scripts.js";
+import { CP437CodeToUTF8Code } from "../DF_Related/ASCIITranslator.js";
 
 async function initial_load() {
     const grid = document.getElementsByClassName("grid")[0];
@@ -33,34 +34,88 @@ async function initial_load() {
     grid.appendChild(outputDiv);
 
     for(let i = 0; i < 9; i++){
-        const aux = function(){
-            let symbol = tranlsateASCII(asciiInputs[i].value);
-            if (symbol.length === 0){
-                symbol = " ";
-            }
+        const updateOutputEvent = function(){
+            const symbol = asciiInputs[i].value;
             const colorText = colorInputs[i].value;
             const [color, backgroundColor] = colorTranslator.translateColors(colorText); 
             updateOutputCell(symbol, color["hex"], backgroundColor["hex"], outputCells[i]);
         }
+
         colorInputs[i].value = "7:0:1";
-        asciiInputs[i].addEventListener("change", aux);
-        colorInputs[i].addEventListener("change", aux);
+        asciiInputs[i].addEventListener("change", updateOutputEvent);
+        colorInputs[i].addEventListener("change", updateOutputEvent);
         asciiInputs[i].dispatchEvent(new Event("change"));
     }
+    
+    const rawDiv = document.getElementById("rawDiv");
+    const stageInput = document.getElementById("stageNum");
+    const rawButton = document.getElementById("rawButton");    
+
+    const rawGenerationEvent = function(){
+        const [tileLines, colorLines] = generateRawStrings(asciiInputs, colorInputs, stageInput.value);
+        updateRawStrings(rawDiv, tileLines, colorLines);
+    }
+
+    rawButton.addEventListener("click", rawGenerationEvent);
 
 }
-
-function tranlsateASCII(text){
+function translateASCII(text){
     if(isNaN(text)){
         return text.replaceAll("\'", "");
-    } else{
-        return String.fromCharCode(parseInt(text));
+    } else{ 
+        const code = parseInt(text)
+        const UTF8Code = CP437CodeToUTF8Code(code);
+        console.log(parseInt(text));
+        
+        return String.fromCharCode(UTF8Code);
     }
 }
 function updateOutputCell(symbol, textColor, backgroundColor, cell){
+    if (symbol.length === 0){
+        symbol = " ";
+    } else {
+        symbol = translateASCII(symbol);
+    }
     cell.innerHTML = symbol;
     cell.style.color = textColor;
     cell.style.background = backgroundColor;
+}
+function updateRawStrings(div, tileLines, colorLines){
+    div.innerHTML = "";
+    const lines = tileLines.concat(colorLines);
+    const p = document.createElement("p");
+
+    for (const line of lines){
+        p.innerHTML += line;
+        p.innerHTML += "<br>";
+    }
+    div.appendChild(p);
+}
+function generateRawStrings(asciiInputs, colorInputs, stage){
+    const tileLines = [];
+    const colorLines = [];
+
+    for (let i = 0; i < 3; i++){
+        let tileLine = `[TILE:${stage}:${i}:`;
+        let colorLine = `[COLOR:${stage}:${i}:`;
+
+        for (let j = 0; j < 3; j++){
+            const index = (i * 3) + j;
+            let symbol = asciiInputs[index].value;
+
+            if ((symbol.length === 0) || (symbol.value === " ")){
+                symbol = "\' \'";
+            }
+
+            tileLine += `${symbol}:`; 
+            colorLine += `${colorInputs[index].value}:`; 
+        }
+        tileLine += "]";
+        colorLine += "]";
+        tileLines.push(tileLine);
+        colorLines.push(colorLine);
+    }
+    return [tileLines, colorLines];
 }
 function make_input_table(title, name, rowNum = 3, columNum = 3) {
     const grid = document.createElement("div");
